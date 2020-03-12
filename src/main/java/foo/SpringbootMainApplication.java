f4a002515debc5c1350db797f04b342603a2e0e2
jcruz <jcruz@tre-pa.jus.br>
2020-03-12 19:29:04 -0300

    Ajustes

--- src/main/java/foo/SpringbootMainApplication.java
+++ src/main/java/foo/SpringbootMainApplication.java
@@ -1,16 +1,26 @@
 package foo;
 
 import foo.api.FS;
+import foo.api.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringbootMainApplication implements CommandLineRunner {
 
     @Autowired
     private FS fs;
+
+    @Autowired
+    private ApplicationContext context;
+
+//    @Autowired(required = false)
+//    private Set<Task> tasks;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMainApplication.class, args);
@@ -19,5 +29,8 @@ public class SpringbootMainApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         fs.copy();
+        Task task = (Task) context.getBean("foo.ModuleATask");
+        task.run();
+//        System.out.println(tasks);
     }
 }
